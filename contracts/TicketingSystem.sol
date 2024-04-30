046debc8a990ba813a6f9e2d1856c9a5b99abf8c
creech911 <163350628+creech911@users.noreply.github.com>
2024-04-30 02:46:25 +0200

    add error catching

--- contracts/TicketingSystem.sol
+++ contracts/TicketingSystem.sol
@@ -13,14 +13,13 @@ contract TicketingSystem is ERC721URIStorage, Ownable {
         uint256 ticketPrice;
         uint256 totalTickets;
         uint256 ticketsSold;
-        address organizer;
     }
 
     mapping(uint256 => Event) public events;
     mapping(uint256 => uint256) public ticketToEvent;
     mapping(address => mapping(uint256 => uint256)) private ownedTicketsCount;
 
-    event EventCreated(uint256 indexed eventId, string eventName, uint256 ticketPrice, uint256 totalTickets, address organizer);
+    event EventCreated(uint256 indexed eventId, string eventName, uint256 ticketPrice, uint256 totalTickets);
     event TicketPurchased(uint256 indexed eventId, uint256 ticketId, address buyer);
     event TicketTransferred(uint256 indexed ticketId, address from, address to);
     event TicketReturned(uint256 indexed eventId, uint256 ticketId, address owner);
@@ -28,20 +27,22 @@ contract TicketingSystem is ERC721URIStorage, Ownable {
     constructor() ERC721("DecentralizedTicket", "DTC") {}
 
     function createEvent(string memory name, uint256 ticketPrice, uint256 totalTickets) public onlyOwner {
+        require(ticketPrice > 0, "Ticket price must be greater than 0");
+        require(totalTickets > 0, "Total tickets must be greater than 0");
         uint256 eventId = nextEventId++;
         events[eventId] = Event({
             id: eventId,
             name: name,
             ticketPrice: ticketPrice,
             totalTickets: totalTickets,
-            ticketsSold: 0,
-            organizer: msg.sender
+            ticketsSold: 0
         });
 
-        emit EventCreated(eventId, name, ticketPrice, totalTickets, msg.sender);
+        emit EventCreated(eventId, name, ticketPrice, totalTickets);
     }
 
     function buyTicket(uint256 eventId) public payable {
+        require(events[eventId].id == eventId, "Event does not exist");
         Event storage event_ = events[eventId];
         require(msg.value == event_.ticketPrice, "Incorrect ticket price");
         require(event_.ticketsSold < event_.totalTickets, "Event is sold out");
@@ -57,10 +58,15 @@ contract TicketingSystem is ERC721URIStorage, Ownable {
     }
 
     function transferTicket(uint256 ticketId, address to) public {
+        require(_exists(ticketId), "Ticket does not exist");
         require(ownerOf(ticketId) == msg.sender, "You must own the ticket to transfer it");
+        require(to != address(0), "Cannot transfer to the zero address");
+
         safeTransferFrom(msg.sender, to, ticketId);
 
         uint256 eventId = ticketToEvent[ticketId];
+        require(events[eventId].id == eventId, "Event does not exist");
+
         ownedTicketsCount[msg.sender][eventId]--;
         ownedTicketsCount[to][eventId]++;
 
@@ -68,10 +74,10 @@ contract TicketingSystem is ERC721URIStorage, Ownable {
     }
 
     function returnTicket(uint256 ticketId) public {
+        require(_exists(ticketId), "Ticket does not exist");
         require(ownerOf(ticketId) == msg.sender, "You must own the ticket to return it");
         uint256 eventId = ticketToEvent[ticketId];
         Event storage event_ = events[eventId];
-        require(block.timestamp < event_.eventDate, "Cannot return ticket after event start");
 
         _burn(ticketId);
         ownedTicketsCount[msg.sender][eventId]--;
