ce37a602f23cca8145468f9dc8a3479c799e944b
creech911 <163350628+creech911@users.noreply.github.com>
2024-05-03 08:07:58 +0200

    new feature

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 const SOLIDITY_VERSION = "0.8.4";
 const PROJECT_ID = process.env.INFURA_PROJECT_ID;
 const PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 module.exports = {
   solidity: {
     version: SOLIDITY_VERSION,
@@ -30,6 +32,10 @@ module.exports = {
       url: `https://goerli.infura.io/v3/${PROJECT_ID}`,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
   },
   paths: {
     sources: "./contracts",
@@ -37,7 +43,18 @@ module.exports = {
     cache: "./cache",
     artifacts: "./artifacts",
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   namedAccounts: {
     deployer: 0,
+  },
+  tasks: {
+    deploy: async (taskArgs, hre) => {
+      const { deployer } = await hre.getNamedAccounts();
+      const Factory = await hre.ethers.getContractFactory("YourContractName", deployer);
+      const contract = await Factory.deploy();
+      console.log("Contract deployed to:", contract.address);
+    }
   },
 };
